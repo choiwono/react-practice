368d1672c1c60bd6269f6a3a77c5972336d5b1d1
choiwono <treasure_b@naver.com>
2019-10-05 15:23:30 +0900

    react life cycle 실습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import ScrollBox from './components/ScrollBox';
+import IterationSample from './components/IterationSample';
+import LifeCycleSample from './components/LifeCycleSample';
+import ErrorBoundary from './components/ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
       <div>
-        <ScrollBox ref={ref => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
+        <button onClick={this.handleClick}>랜덤색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}/>
+        </ErrorBoundary>
       </div>
     );
   }
