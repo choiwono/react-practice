aa8b8343ad422fdf7a7cab5aeaa6bdf6ff8e05be
choiwono <treasure_b@naver.com>
2019-10-05 14:05:09 +0900

    # react ref 연습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  const name = "리액트수였습니다 짜짠";
-  return (
+import React, { Component } from 'react';
+import ScrollBox from './components/ScrollBox';
+class App extends Component {
+  render() {
+    return (
       <div>
-        { name === "리액트" ? ( <h1>리액트입니다.</h1> ) : ( <h2>리액트 아닙니다.</h2> ) }
+        <ScrollBox ref={ref => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
       </div>
-  );
+    );
+  }
 }
-
 export default App;
