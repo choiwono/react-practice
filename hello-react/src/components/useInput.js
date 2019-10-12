9e70a12d4711f2489d285c9f833f9f3172124991
choiwono <treasure_b@naver.com>
2019-10-12 23:01:34 +0900

    no message

---
+++ hello-react/src/components/useInput.js
@@ -0,0 +1,13 @@
+import React, { use } from 'react';
+
+class useInput extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default useInput;
