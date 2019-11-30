2098bb70086520418f9f85f2aeed8d0b6828e918
choiwono <treasure_b@naver.com>
2019-11-30 15:47:09 +0900

    axios 추가

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -3,10 +3,23 @@ import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+       const response = await axios.get(
+         'https://newsapi.org/v2/top-headlines?country=kr&apiKey=ddb44ba0247a406088ad0e17183fb4b6',
+       );
+       setData(response.data);
+    } catch(e) {
+       console.log(e);
+    }  
+  };
 
   return (
     <div>
-      
+      <div>
+        <button onClick={onClick}>불러오기</button>
+        {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+      </div>
     </div>
   );
 };
