3e8d356deb913c602d55bb6644677057dc59e3d2
choiwono <treasure_b@naver.com>
2019-11-30 15:35:08 +0900

    newsViewers 연습

---
+++ news-viewer/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default App;
--- router-tutorial/src/Profile.js
+++ router-tutorial/src/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import WithRouterSample from './WithRouterSample';
 
 const data = {
     velopert: {
@@ -22,6 +23,7 @@ const Profile = ({ match }) => {
         <div>
             <h3>{username}({profile.name})</h3>
             <p>{profile.description}</p>
+            <WithRouterSample/>
         </div>
     );
 };
