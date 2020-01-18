a3868e489ba986d23d367f7e90388b733dd446c4
choiwono <treasure_b@naver.com>
2020-01-18 16:13:53 +0900

    redux-middleware 실습, 서버사이드 렌더링 연습

---
+++ learn-redux-middleware/src/lib/loggerMiddleware.js
@@ -0,0 +1,19 @@
+/*const loggerMiddleware = function loggerMiddleware(store) {
+    return function(next) {
+        return function(action) {
+
+        }
+    }
+}*/
+
+
+const loggerMiddleware = store => next => action => {
+    console.group(action && action.type);
+    console.log('이전상태', store.getState());
+    console.log('액션', action);
+    next(action);
+    console.log('다음 상태', store.getState())
+    console.groupEnd();
+};
+
+export default loggerMiddleware;
---
+++ learn-redux-middleware/src/modules/sample.js
@@ -0,0 +1,5 @@
+import { handleActions } from 'redux-actions';
+import * as api from '../lib/api';
+
+const GET_POST = 'sample/GET_POST';
+const GET_POST_SUCESS = 'sample/GET_POST_SUCCESS'
